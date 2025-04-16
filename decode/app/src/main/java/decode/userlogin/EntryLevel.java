a46f4fabae16f83a6089d26acb1a81154712efcf
Dlafferty251 <dlafferty251@gmail.com>
2025-04-16 21:37:26 +0000

    feat: Creates a basic file structure design for our project

---
+++ decode/app/src/main/java/decode/userlogin/EntryLevel.java
@@ -0,0 +1,8 @@
+package decode.userlogin;
+
+/**
+ * This class represents an EntryLevel user in the Decode application.
+ */
+public class EntryLevel {
+    
+}
---
+++ decode/app/src/main/java/decode/userlogin/UserRegistration.java
@@ -0,0 +1,16 @@
+package decode.userlogin;
+
+/**
+ * This class will be used to register users in the Decode application.
+ * It will handle the registration process for different user types.
+ * The user types include Subject Matter Expert, BootCampee, and EntryLevel.
+ */
+
+
+ /**
+  * This class is currently abstract because I am thinking that we may want to extend it
+  to create different levels for the users later on. SUBJECT TO CHANGE
+  */
+public abstract class UserRegistration {
+    
+}
