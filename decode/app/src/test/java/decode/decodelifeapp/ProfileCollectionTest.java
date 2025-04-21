739a82d4c3a6c071c50c0380c6c4de7623579768
Meiko-S22 <mdstephens251@gmail.com>
2025-04-21 15:20:26 +0000

    fix: adds gender

--- decode/app/src/test/java/decode/decodelifeapp/ProfileCollectionTest.java
+++ decode/app/src/test/java/decode/decodelifeapp/ProfileCollectionTest.java
@@ -7,7 +7,7 @@ public class ProfileCollectionTest {
     void testPrintProfile(){
         ProfileCollection profileCollection = new ProfileCollection();
         profileCollection.createProfileList();
-       profileCollection.addProfile("John Smith", "student", 5551234567L, "johnsmith@gmail.com");
+       profileCollection.addProfile("John Smith", "student", "male", 5551234567L, "johnsmith@gmail.com");
         profileCollection.printProfiles();
         assertEquals("John Smith", profileCollection.profileNames.get(0));
         assertEquals("student", profileCollection.profileRoles.get(0));
@@ -19,7 +19,7 @@ public class ProfileCollectionTest {
     void testAddProfile(){
         ProfileCollection profileCollection = new ProfileCollection();
         profileCollection.createProfileList();
-        profileCollection.addProfile("John Smith", "student", 5551234567L, "johnsmith@gmail.com");
+        profileCollection.addProfile("John Smith", "student","male", 5551234567L, "johnsmith@gmail.com");
         assertEquals("John Smith", profileCollection.profileNames.get(0));
         assertEquals("student", profileCollection.profileRoles.get(0));
         assertEquals(5551234567L, profileCollection.profilePhoneNumbers.get(0).longValue());
