8514f47511dbda29ce231534d78b2a3f58302e68
Meiko Stephens <mdstephens251@gmail.com>
2025-04-21 09:30:39 -0400

    feat: adds profile collection (#16)
    Signed off by: Dylan Lafferty

---
+++ decode/app/src/test/java/decode/decodelifeapp/ProfileCollectionTest.java
@@ -0,0 +1,9 @@
+package decode.decodelifeapp;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import org.junit.jupiter.api.Test;
+
+public class ProfileCollectionTest {
+    @Test
+    
+}
