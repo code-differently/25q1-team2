c5e6b641e33b3a2ace5650075e9c64b1395c2ae8
Dylan Lafferty <dlafferty251@gmail.com>
2025-05-12 12:46:40 -0400

    feat: installs nextjs (#33)
    * feat: installs nextjs
    
    * feat: correctly installs next
    
    * feat: installs nextjs template
    
    * feat: initialize Next.js project with configuration and assets
    
    * feat: remove unused nextjs module and add new project configuration
    
    * feat: update package.json and package-lock.json to include TypeScript and type definitions for Node and React

--- decode-interviews/server/src/routes/questions.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAllQuestions } from '../controllers/questionController.js';
-
-const router = express.Router();
-
-// GET route for fetching all questions
-router.get('/', getAllQuestions);
-
-export default router;
