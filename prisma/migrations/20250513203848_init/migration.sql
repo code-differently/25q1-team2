e9a487f36e8cf83f9c54d94f441bf7f5f4bed7ca
Dlafferty251 <dlafferty251@gmail.com>
2025-05-13 20:42:49 +0000

    Feat: update dependencies and enhance question model with difficulty field

--- prisma/migrations/20250513203848_init/migration.sql
+++ prisma/migrations/20250513203848_init/migration.sql
@@ -3,5 +3,6 @@ CREATE TABLE "Question" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "text" TEXT NOT NULL,
     "answer" TEXT NOT NULL,
+    "difficulty" TEXT NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
